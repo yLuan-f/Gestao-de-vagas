f8feec099b823abce283c5a1747df846015476ef
yLuan-f <yluanfarias2006@gmail.com>
2026-07-14 09:42:43 -0300

    inserindo validacoes

--- src/main/java/br/com/gestao/vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/gestao/vagas/modules/candidate/CandidateEntity.java
@@ -1,7 +1,11 @@
 package br.com.gestao.vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -10,9 +14,17 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] nao deve conter espacos")
     private String username;
+
+    @Email(message = "Por favor, insira um [email] válido")
     private String email;
+
+    @Length(min = 4, max = 8, message = "A senha deve conter entre 4 e 8 caracteres")
     private String password;
+
     private String description;
     private String curriculum;
 
